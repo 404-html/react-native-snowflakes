724f49121882f2100e83905dde6545bfe68b358d
arturd@xara.com <arturd@xara.com>
2019-12-08 23:28:03 +0100

    The package itself!

---
+++ index.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { Animated, View, Dimensions, Easing } from 'react-native';
+
+const Snowflake = (props) => {
+
+	const { width, height } = Dimensions.get('window');
+	let x = Math.round(Math.random() * (width));
+	const [xAnim] = useState(new Animated.Value(x));
+	const [yAnim] = useState(new Animated.Value(0));
+
+	const lifeTime = 20000;
+	const horizontalMoveDuringLifeTime = 6;
+	const animationDelay = Math.round(Math.random() * (20000));
+
+	const animations = [];
+	const maxHorizontalMove = width / 10;
+	for (let i = 0; i < horizontalMoveDuringLifeTime; i++) {
+		const horizontalMove = Math.round(Math.random() * (maxHorizontalMove));
+		x += (i % 2 === 0) ? horizontalMove : - horizontalMove;
+		animations.push(Animated.timing(
+			xAnim, {
+				toValue: x,
+				duration:  lifeTime / horizontalMoveDuringLifeTime,
+				delay: i === 0 ? animationDelay : 0
+			}
+		));
+	}
+
+	useEffect(() => {
+
+		Animated.loop(
+			Animated.timing(
+				yAnim,
+				{
+					toValue: height + 40,
+					duration: 20000,
+					easing: Easing.linear,
+					delay: animationDelay
+				}
+			)
+		).start();
+
+		Animated.loop(
+			Animated.sequence(animations)
+		).start();
+
+	}, []);
+
+	return (
+		<Animated.Image
+			style={{
+				...props.style,
+				position: 'absolute',
+				left: xAnim,
+				top: yAnim,
+				height: 20,
+				width: 20,
+				opacity: .6
+			}}
+			source={require('./snowflake.png')}
+			resizeMode="cover"
+		>
+		</Animated.Image>
+	);
+}
+
+export default () => {
+	const { height } = Dimensions.get('window');
+
+	return (
+		<View pointerEvents="none" style={{
+			flex: 1,
+			position: 'absolute',
+			width: '100%',
+			top: -20,
+			height: height + 40
+		}}>
+			{[...Array(20)].map((e, i) => <Snowflake key={i} />)}
+		</View>
+	)
+}
